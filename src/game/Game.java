929cf1e929fdb2bb6c7cb63e015a7a991ffa63ae
SujitBhatta-404 <102691694+SujitBhatta21@users.noreply.github.com>
2024-02-19 14:28:05 +0000

    Bugfixes: Fixed my bug with keyboard listener. The ball moves if arrow key is pressed.
    Biggest challange for this was me not realising there is Walker class in city engine. Spent hrs making the character jump. I was successful but the gravity was interfering with my ball. Using given methods of Walker i can easily move the ball easily.

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,7 +4,7 @@ import city.cs.engine.*;
 import city.cs.engine.Shape;
 import org.jbox2d.common.Vec2;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,8 +13,6 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 /**
  * Your main game entry point
@@ -23,6 +21,7 @@ import java.awt.event.KeyEvent;
 
 public class Game {
 
+    private static Ball ball;  // Making ball a static field.
 
     /** Initialise a new Game. */
     public Game() {
@@ -38,21 +37,14 @@ public class Game {
         final JFrame frame = new JFrame("City Game");
         frame.setSize(WIDTH, HEIGHT); // Set the size of the JFrame
 
+        // Testing if the order of code matters.
+        level_1(world, frame);
+
         // Set the layout manager to null::: Personally I did not like default layer manager.
         frame.setLayout(null);
 
         view.setBounds(0, 0, WIDTH, HEIGHT);
         frame.add(view); // Add the game view to the frame
-
-        // Testing button
-        Color BLACK = new Color(255, 100, 255);
-        Button start_button = new Button(10, 10, 100, 30, BLACK, "Start Button", new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("BUTTON IS CLICKED!!");
-            }
-        });
-        frame.add(start_button.getButton()); // Add the button to the frame
 
         // enable the frame to quit the application
         // when the x button is pressed
@@ -64,17 +56,16 @@ public class Game {
         // finally, make the frame visible
         frame.setVisible(true);
 
-        //optional: uncomment this to make a debugging view
-        // JFrame debugView = new DebugViewer(world, 500, 500);
+        // request focus in window. Allows keyListener to work.
+        frame.requestFocusInWindow();
 
-        // Checks if a key is pressed.
-        KeyboardListener(frame);
-
-        level_1(world, frame);
+        // Making a debugging view. Used for debugging.
+        JFrame debugView = new DebugViewer(world, 500, 500);
 
         // start our game world simulation!
         world.start();
     }
+
 
 
     /** Run the game. */
@@ -95,34 +86,32 @@ public class Game {
         Shape platformShape = new BoxShape(3, 0.5f);
         StaticBody platform1 = new StaticBody(world, platformShape);
         platform1.setPosition(new Vec2(-8, -4f));
-        platform1.addImage(new BodyImage("assets/images/Platform.png"));
+        // platform1.addImage(new BodyImage("assets/images/Platform.png")); (Find a good background after milestone completed.....)
 
         //make a character (with an overlaid image)
 
-        Ball ball = new Ball(world);
-        ball.setPosition(new Vec2(GlobalVariables.getXPos(), GlobalVariables.getYPos()));
+        ball = new Ball(world, -4, 5);
+        ball.setPosition(new Vec2(Ball.getXPos(), Ball.getYPos()));
+        System.out.println(ball.getGravityScale());
 
-    }
+        // Checks if a key is pressed.
+        KeyboardListener k = new KeyboardListener(ball);
+        frame.addKeyListener(k);
 
-    public static void KeyboardListener(JFrame myFrame) {
-        myFrame.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                if (keyCode == KeyEvent.VK_UP) {
-                    System.out.println("Up Arrow-Key is pressed!");
-                    GlobalVariables.setYPos(GlobalVariables.getYPos() + 1);
-                } else if (keyCode == KeyEvent.VK_DOWN) {
-                    System.out.println("Down Arrow-Key is pressed!");
-                } else if (keyCode == KeyEvent.VK_LEFT) {
-                    System.out.println("Left Arrow-Key is pressed!");
-                    GlobalVariables.setXPos(GlobalVariables.getXPos() - 1);
-                } else if (keyCode == KeyEvent.VK_RIGHT) {
-                    System.out.println("Right Arrow-Key is pressed!");
-                    GlobalVariables.setXPos(GlobalVariables.getXPos() + 1);
-                }
-            }
-        });
+        JButton test = new JButton("Test Button");
+        test.setBounds(50,100,95,30);
+        frame.add(test);
+
+
+//        // Testing button
+//        Color BLACK = new Color(255, 100, 255);
+//        Button start_button = new Button(10, 10, 100, 30, BLACK, "Start Button", new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("BUTTON IS CLICKED!!");
+//            }
+//        });
+//        frame.add(start_button.getButton()); // Add the button to the frame
+
     }
 }
-
-
---
+++ src/game/KeyboardListener.java
@@ -0,0 +1,47 @@
+package game;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardListener implements KeyListener{
+    private Ball ball;
+    private static float ball_speed = 10;
+
+    KeyboardListener(Ball ball) {
+        this.ball = ball;
+    }
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            System.out.println("Up Arrow-Key is pressed!");
+            ball.jump(ball_speed);
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            System.out.println("DOWN Arrow-Key is pressed!");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("LEFT Arrow-Key is pressed!");
+            ball.startWalking(-ball_speed);
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Right Arrow-Key is pressed!");
+            ball.startWalking(ball_speed);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            // Stop the ball's movement when left or right key is released
+            ball.stopWalking();
+        }
+        // You can add more conditions based on your requirements
+    }
+}
